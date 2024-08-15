a84c324ad04b134e3415d01bf80e1f695adf5492
alyona <akovalcuk485@gmail.com>
2024-08-15 16:33:11 +0300

    finish markup

--- js/task-2.js
+++ js/task-2.js
@@ -1,27 +1,25 @@
-class Storage {
-  #items;
-  constructor(items) {
-    this.#items = items;
-  }
-  getItems() {
-    return this.#items;
-  }
-  addItem(newItem) {
-    this.#items.push(newItem);
-  }
-  removeItem(itemToRemove) {
-    this.#items = this.#items.filter(item => item !== itemToRemove);
-  }
-}
+const images = [
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+const gallery = document.querySelector('.gallery');
 
-const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+const template = images
+  .map(
+    ({ url, alt }) => `<li>
+        <img src="${url}" alt="${alt}" width='400'>
+      </li>`
+  )
+  .join('');
 
-storage.addItem('Droid');
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-
-storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
-
-storage.removeItem('Scaner');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+gallery.insertAdjacentHTML('beforeend', template);
--- js/task-3.js
+++ js/task-3.js
@@ -1,27 +1,9 @@
-class StringBuilder {
-  #value;
-  constructor(initialValue) {
-    this.#value = initialValue;
+const input = document.querySelector('#name-input');
+const nameUser = document.querySelector('#name-output');
+const helloName = event => {
+  nameUser.textContent = event.currentTarget.value.trim();
+  if (!nameUser.textContent) {
+    nameUser.textContent = 'Anonymous';
   }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value = str.concat(this.#value);
-  }
-  padStart(str) {
-    this.#value = this.#value.concat(str);
-  }
-  padBoth(str) {
-    this.#value = str.concat(this.#value).concat(str);
-  }
-}
-
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+};
+input.addEventListener('input', helloName);
